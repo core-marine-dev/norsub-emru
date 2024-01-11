ff5cc9a2dd48804439fbb2e3f63fe9ea55daf49c
Cristóbal Contreras Rubio <crisconru@gmail.com>
2024-01-11 16:24:31 +0100

    parser tested with fully coverage

--- src/parser.ts
+++ src/parser.ts
@@ -1,13 +1,87 @@
 import path from 'node:path'
 import { NMEAParser } from '@coremarine/nmea-parser'
+import { FieldParsed, NMEALike, NMEASentence, ProtocolOutput, ProtocolsInput, Sentence } from '@coremarine/nmea-parser/lib/types'
+import { BooleanSchema, NaturalSchema } from '@coremarine/nmea-parser/lib/schemas'
+import { MAX_CHARACTERS } from '@coremarine/nmea-parser/lib/constants'
+import { getUint32 } from './utils'
+import { getStatus } from './status'
+import { NorsubSentence } from './types'
 
+export class NorsubParser {
+  // Parser
+  protected _parser: NMEAParser = new NMEAParser()
+  // Memory - Buffer
+  get memory() { return this._parser.memory }
+  set memory(mem: boolean) { this._parser.memory = BooleanSchema.parse(mem) }
+  get bufferLimit() { return this._parser.bufferLimit }
+  set bufferLimit(limit: number) { this._parser.bufferLimit = NaturalSchema.parse(limit) }
 
-export class NorsubParser extends NMEAParser {
-
-  constructor(memory: boolean = true) {
-    
-    super(memory)
+  constructor(memory: boolean = true, limit: number = MAX_CHARACTERS) {
+    this.memory = memory
+    this.bufferLimit = limit
     const NORSUB_FILE = path.join(__dirname, 'norsub.yaml')
     this.addProtocols({ file: NORSUB_FILE })
   }
+
+  private getStatusIndexes(fields: FieldParsed[]): number[] {
+    let indexes: number[] = []
+    fields.forEach((field, index) => {
+      if (field.name.includes('status')) {
+        indexes.push(index)
+      }
+    })
+    return indexes
+  }
+
+  private addStatus(nmea: NMEASentence): NorsubSentence {
+    const indexes: number[] = this.getStatusIndexes(nmea.fields)
+    const numberOfIndex = indexes.length
+    if (![1, 2].includes(numberOfIndex)) { return nmea }
+    const sentence: NorsubSentence = { ...nmea }
+    // Status
+    if (numberOfIndex === 1) {
+      const index = indexes[0]
+      const data = sentence.fields[index].data as number
+      const status = getStatus({ status: data })
+      if (status !== null) {
+        sentence.fields[index].metadata = status
+      }
+      return sentence
+    }
+    // Status_A + Status_B
+    const [index_a, index_b] = indexes
+    const status_a = sentence.fields[index_a].data as number
+    const status_b = sentence.fields[index_b].data as number
+    const status = getStatus({ status_a, status_b })
+    if (status !== null) {
+      const statusNumber = getUint32(status_a, status_b)
+      sentence.data.push(statusNumber)
+      sentence.fields.push({
+        name: 'status',
+        type: 'uint32',
+        data: statusNumber,
+        metadata: status
+      })
+    }
+    return sentence
+  }
+
+  parseData(data: string): NorsubSentence[] {
+    const sentences = this._parser.parseData(data)
+    if (sentences.length === 0) return sentences
+    return sentences.map(sentence => {
+      if (sentence.protocol.name.includes('NORSUB')) {
+        return this.addStatus(sentence)
+      }
+      return sentence
+    })
+  }
+
+  addProtocols(protocols: ProtocolsInput): void { this._parser.addProtocols(protocols) }
+  
+  getProtocols(): ProtocolOutput[] { return this._parser.getProtocols() }
+
+  getSentence(id: string): Sentence { return this._parser.getSentence(id) }
+
+  getFakeSentenceByID(id: string): NMEALike | null { return this._parser.getFakeSentenceByID(id) }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,8 @@
 import { describe, test, expect } from 'vitest'
 
 import { NorsubParser } from '../src'
+import { ProtocolOutput, Sentence } from '@coremarine/nmea-parser/lib/types'
+import { StatusSchema } from '../src/schemas'
 
 const PROTOCOLS = ['NMEA', 'GYROCOMPAS1', 'NORSUB', 'NORSUB2', 'NORSUB6', 'NORSUB7', 'NORSUB7b', 'NORSUB8', 'NORSUB PRDID', 'Tokimek PTVG', 'RDI ADCP', 'SMCA', 'SMCC']
 
@@ -12,6 +14,32 @@ describe('Parser', () => {
     const parserProtocols = parser.getProtocols()
     parserProtocols.forEach(({ protocol }) => {
       expect(PROTOCOLS.includes(protocol)).toBeTruthy()
+    })
+  })
+
+  test('Norsub Sentences', () => {
+    const parser = new NorsubParser()
+    const output = parser.getProtocols()
+    output.forEach((element: ProtocolOutput) => {
+      if (!element.protocol.includes('NMEA')) {
+        element.sentences.forEach(sentence => {
+          const norsubSentence: Sentence = parser.getSentence(sentence)
+          expect(norsubSentence).not.toBeNull()
+          if (norsubSentence !== null) {
+            const fake = parser.getFakeSentenceByID(norsubSentence.sentence)
+            expect(fake).not.toBeNull()
+            if (fake !== null) {
+              const parsed = parser.parseData(fake)
+              expect(parsed).toHaveLength(1)
+              const norsub = parsed[0]
+              if (norsub.sentence.includes('PNORSUB')) {
+                const statusField = (norsub.fields.slice(-1))[0].metadata
+                expect(StatusSchema.safeParse(statusField).success).toBeTruthy()
+              }
+            }
+          }
+        })
+      }
     })
   })
 })
